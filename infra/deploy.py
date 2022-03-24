60fe0d523f275c95e04675eb5c761c418124097d
Bede Kelly <bede.kelly@alpima.com>
2022-03-24 15:22:12 +0000

    Try checking out stuff

--- infra/deploy.py
+++ infra/deploy.py
@@ -5,8 +5,19 @@ Figure out the changes since the last commit on this branch that changed the
 environment file.
 """
 import os
+import json
 from git import Repo
 
-repo = Repo(os.getcwd())
 
-print(repo)
+repo = Repo(os.getcwd(), search_parent_directories=False)
+
+
+with open("infra/environments.json") as environments_file:
+    environments_obj = json.loads(environments_file.read())
+    environments = environments_obj["environments"]
+    for environment, environment_options in environments.items():
+        target = environment_options["checkout"]
+        print(f"Deploying '{environment}' using git target <{target}>")
+        repo.git.checkout(target)
+        print(list(os.listdir()))
+        
