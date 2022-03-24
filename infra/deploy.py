a35917f4eed139075fc4032825aee789e87352fd
Bede Kelly <bede.kelly@alpima.com>
2022-03-24 15:32:41 +0000

    Test different env file

--- infra/deploy.py
+++ infra/deploy.py
@@ -3,6 +3,9 @@ deploy.py
 
 Figure out the changes since the last commit on this branch that changed the
 environment file.
+
+Run from the root directory, as:
+    $ python3 infra/deploy.py
 """
 import os
 import json
@@ -11,13 +14,31 @@ from git import Repo
 
 repo = Repo(os.getcwd(), search_parent_directories=False)
 
+repo.git.checkout("main")
 
-with open("infra/environments.json") as environments_file:
-    environments_obj = json.loads(environments_file.read())
-    environments = environments_obj["environments"]
+
+def get_past_environments():
+    repo.git.checkout("HEAD^1")
+    past_environments = get_environments();
+    repo.git.checkout("-")
+    return past_environments
+
+
+def get_environments():
+    with open("infra/environments.json") as environments_file:
+        environments_obj = json.loads(environments_file.read())
+        environments = environments_obj["environments"]
+        return environments
+
+
+def deploy_environments(environments):
     for environment, environment_options in environments.items():
         target = environment_options["checkout"]
         print(f"Deploying '{environment}' using git target <{target}>")
         repo.git.checkout(target)
-        print(list(os.listdir()))
         
+
+
+print(get_past_environments())
+repo.git.checkout("main")
+print(get_environments())
