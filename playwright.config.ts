bcf2ce390602e00a704ce0b732bdaad3e4b5fcc3
Bede Kelly <contact@bede.io>
2022-03-31 17:33:00 +0100

    Don't depend on dist folder for integration tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,9 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "integration",
   webServer: {
-    command: "make serve-existing",
+    command: "make start",
     port: 3000,
-    timeout: 20 * 1000,
+    timeout: 5000,
     reuseExistingServer: false,
   },
 };
