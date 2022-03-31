8f15d18d8d92b2d4439aa09911e9b5eaa7e57ff4
Bede Kelly <contact@bede.io>
2022-03-31 17:12:33 +0100

    View errors live in vite window as well as browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: { lintCommand: "eslint ./src/**/*.{ts,tsx}" },
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
